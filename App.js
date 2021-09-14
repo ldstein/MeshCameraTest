27440edf040fcbbb592af6b1e32e8bd38561147e
ldstein <ldstein@gmail.com>
2021-09-14 16:10:18 +1000

    Added react-native-vision-camera

--- App.js
+++ App.js
@@ -6,26 +6,21 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, {
+    useState,
+    useCallback,
+    useMemo,
+    useEffect,
+} from 'react';
+
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Button,
+  StyleSheet,  
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Camera, useCameraDevices, useCameraFormat, useFrameProcessor} from 'react-native-vision-camera'
 
 import {runOnJS, runOnUI} from 'react-native-reanimated';
 
@@ -44,88 +39,52 @@ function onTestWorkletClick() {
     runOnUI(someWorklet)('Howdy');
 }
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+function CameraView()
+{
+    const [hasPermission, setHasPermission] = useState(false);
+    const cameraDevices = useCameraDevices();
+    const cameraDevice = cameraDevices.back;
+    const format = useCameraFormat(cameraDevice);
+
+    const frameProcessor = useFrameProcessor(function(frame)
+    {
+        'worklet';
+        console.log("Processing Frame", frame.toString());
+    }, []);
+
+    useEffect(function()
+    {
+        async function onMount()
+        {
+            const permission = await Camera.requestCameraPermission();
+            console.log('xxx Permission?', permission);
+            setHasPermission(permission === 'authorized');
+        }
+        onMount();
+    }, []);
+
+    if (!hasPermission || !cameraDevice)
+        return null;
+
+    return (
+        <Camera
+            style={ StyleSheet.absoluteFill }
+            device={cameraDevice}
+            format={format}
+            isActive={true}
+            frameProcessor={frameProcessor}
+            frameProcessorFps={1}
+        />
+    )    
+}
+
+function App()
+{
   return (
-    <View style={styles.sectionContainer}>      
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <>
+        <CameraView />
+    </>
   );
 };
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Button title="Test Worklet" onPress={onTestWorkletClick} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
